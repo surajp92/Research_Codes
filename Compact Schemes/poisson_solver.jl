9b2dface617cafd957e5a9158dffcbf9039ac6ac
surajp92 <surajp92@vt.edu>
2019-02-19 11:35:42 -0600

    Input from text file feature done

--- Compact Schemes/poisson_solver.jl
+++ Compact Schemes/poisson_solver.jl
@@ -1,13 +1,36 @@
-# using DelimitedFiles
-# file_input = open("input.txt")
-# nx = parse(Int,split(readline(file_input), "#"))
-# close(file_input)
-nx = 128
-ny = 128
-initial_problem = 1
+
+# read data from text file for input parameters
+file_input = open("input.txt")
+input_lines = readlines(file_input)
+input_parameters = Array{Float32}(undef, 13)
+counter = 1
+for line in input_lines
+    m = match(r"^([0-9]+)", line)
+    input_parameters[counter] = parse(Float32, m[1])
+    global counter += 1
+end
+
+close(file_input)
+
+# asssign input parameters
+nx                      = Int32(input_parameters[1])
+ny                      = Int32(input_parameters[1])
+flag_problem            = Int32(input_parameters[2])
+flag_solver             = Int32(input_parameters[3])
+flag_multigrid          = Int32(input_parameters[4])
+flag_start              = Int32(input_parameters[5])
+flag_restriction        = Int32(input_parameters[6])
+tolerance               = Float32(input_parameters[7])
+omega                   = Float32(input_parameters[8])
+relaxation_f2c          = Int32(input_parameters[9])
+relaxaation_c2f         = Int32(input_parameters[10])
+relazation_coarsest     = Int32(input_parameters[11])
+maximum_iterations      = Int32(input_parameters[12])
+tiny                    = Float32(input_parameters[13])
+k = 1
 
 # Assign the domain size based on initial problem
-if initial_problem == 1
+if problem == 1
     x_left      = -1.0
     x_right     = 1.0
 
@@ -38,5 +61,3 @@ end
 for i = 1:ny+1
     y_position[i] = y_bottom + dy*(i-1)
 end
-
-#
