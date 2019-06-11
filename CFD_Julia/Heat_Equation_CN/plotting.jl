b50890d6a2c61e54aa1adffe5587a0ca085c5126
surajp92 <surajp92@vt.edu>
2019-06-11 11:04:30 -0500

    heat equation plotting scripts cleaned

--- CFD_Julia/Heat_Equation_CN/plotting.jl
+++ CFD_Julia/Heat_Equation_CN/plotting.jl
@@ -2,7 +2,7 @@ clearconsole()
 
 using CSV
 using Plots
-font = Plots.font("Times New Roman", 20)
+font = Plots.font("Times New Roman", 18)
 pyplot(guidefont=font, xtickfont=font, ytickfont=font, legendfont=font)
 
 final_field = CSV.read("field_final.csv")#, datarow = 2, type=Float64)
@@ -17,14 +17,33 @@ u = Array{Float64}(undef, length(u_e), 2)
 u[:,1] = u_e
 u[:,2] = u_n
 
+for i = 1:Int64(length(u_error))
+    u_error[i] = abs(u_error[i])
+end
+
 # plotting the exact and numerical solution
 styles = [:solid, :dot]
 styles = reshape(styles, 1, length(styles))
 color=[:red :blue]
 
-p1 = plot(x,u, line = (6,styles), xlabel="\$X\$", ylabel = "\$U\$",  xlims=(minimum(x),maximum(x)),
-     grid=(:none), linestyle=:dot, label=["Exact" "Numerical"], color=color)
-     #legendfontsize=15, xtickfontsize=15, ytickfontsize=15, guidefontsize=15)
+p1 = plot(x,u, line = (6,styles), color=color,
+               xlabel="\$X\$", xlims=(minimum(x),maximum(x)),
+               ylabel = "\$U\$",
+               grid=(:none), label=["Exact solution" "ICP scheme"],
+               legend=:topright)
 
-plot(p1, size = (600, 400))
-savefig(p1,"cn.pdf")
+plot(p1, size = (800, 400))
+savefig("cn.pdf")
+
+p2 = plot(x,u_error, line = (3,styles), linestyle=:dot, color=:green,
+                     xlabel="\$X\$", xlims=(minimum(x),maximum(x)),
+                     ylabel="\$ϵ\$", grid=(:none),
+                     label="Error", legend=:topright,
+                     markershape = :circle, markercolor = :green,
+                     markerstrokecolor = :black, markersize = 7)
+
+plot(p2, size = (400, 400))
+savefig("cn_error.pdf")
+
+plot(p1,p2, size=(1400,500))
+savefig("cn_all.pdf")
